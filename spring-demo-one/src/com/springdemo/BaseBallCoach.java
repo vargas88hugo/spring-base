7ed95d7d4f443330ea24fafdefae7b713c2bf62c
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-28 17:54:25 -0500

    [Feature] Implemented Dependency Injection in the classes! Very nice

--- spring-demo-one/src/com/springdemo/BaseBallCoach.java
+++ spring-demo-one/src/com/springdemo/BaseBallCoach.java
@@ -2,8 +2,19 @@ package com.springdemo;
 
 public class BaseBallCoach implements Coach {
 	
+	private FortuneService fortuneService;
+	
+	public BaseBallCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 }
--- spring-demo-one/src/com/springdemo/Coach.java
+++ spring-demo-one/src/com/springdemo/Coach.java
@@ -2,4 +2,6 @@ package com.springdemo;
 
 public interface Coach {
 	public String getDailyWorkout();
+	
+	public String getDailyFortune();
 }
