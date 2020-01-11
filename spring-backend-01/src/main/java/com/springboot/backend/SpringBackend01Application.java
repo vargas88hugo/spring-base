60df2f077759b93bc444cf58f3fe54d6dc8714f7
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-10 19:46:17 -0500

    [Feature] Database connection established and configure clientes table

---
+++ spring-backend-01/src/main/java/com/springboot/backend/SpringBackend01Application.java
@@ -0,0 +1,13 @@
+package com.springboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackend01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackend01Application.class, args);
+	}
+
+}
---
+++ spring-backend-01/src/test/java/com/springboot/backend/SpringBackend01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackend01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
